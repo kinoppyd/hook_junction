3ba7adc80971ecca9df4ae35e1d7dd45d60ea2bb
Yasuki <yasuki@tolarian-academy.net>
2014-04-16 18:29:50 +0900

    HookReceiverの動きを何となく定義？

--- lib/Model/HookReceiver.pm
+++ lib/Model/HookReceiver.pm
@@ -1,11 +1,38 @@
 package Model::HookReceiver;
+use Mouse::Util::TypeConstraints;
+enum 'HOOK_RECEIVER_DATA_TYPE' => qw/JSON XML/;
+no Mouse::Util::TypeConstraints;
+
 use Mouse::Role;
 
-requires qw/parse toString/;
+requires qw/parse_json parse_xml/;
 
 has 'receivedData' => (
     is => 'ro',
-    isa => 'String'
+    isa => 'Str',
+    init_arg => 'received_data'
 );
+
+has 'type' => (
+    is => 'ro',
+    isa => 'HOOK_RECEIVER_DATA_TYPE',
+    init_arg => 'type'
+);
+
+has 'attr' => (
+    is => 'rw',
+    isa => 'HashRef',
+);
+
+sub parse {
+    my ($self) = @_;
+
+    if($self->type eq 'JSON'){
+        $self->perse_json();
+    } elsif ($self->type eq 'XML'){
+        $self->parse_xml();
+    }
+    $self;
+}
 
 1;
