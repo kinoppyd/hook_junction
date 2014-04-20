b153efbb68239842e0daa913e23ee80d43b66100
Kinoshita.Yasuhiro <kinoshita.yasuhiro@redfox.co.jp>
2014-04-20 20:53:07 +0900

    GithubのHookReceiverに、各Hookを追加

--- lib/Model/HookReceiver/GitHub.pm
+++ lib/Model/HookReceiver/GitHub.pm
@@ -2,4 +2,80 @@ package Model::HookReceiver::GitHub;
 use Mouse;
 with qw/HookReceiver/;
 
+sub commit_comment {
+    my ($self, $arg) = @_;
+}
+
+sub create {
+    my ($self, $arg) = @_;
+}
+
+sub delete {
+    my ($self, $arg) = @_;
+}
+
+sub deployment_status {
+    my ($self, $arg) = @_;
+}
+
+sub deployment {
+    my ($self, $arg) = @_;
+}
+
+sub fork {
+    my ($self, $arg) = @_;
+}
+
+sub gollum {
+    my ($self, $arg) = @_;
+}
+
+sub issue_comment {
+    my ($self, $arg) = @_;
+}
+
+sub issues {
+    my ($self, $arg) = @_;
+}
+
+sub member {
+    my ($self, $arg) = @_;
+}
+
+sub page_build {
+    my ($self, $arg) = @_;
+}
+
+sub public {
+    my ($self, $arg) = @_;
+}
+
+sub pull_request_review_comment {
+    my ($self, $arg) = @_;
+}
+
+sub pull_request {
+    my ($self, $arg) = @_;
+}
+
+sub push {
+    my ($self, $arg) = @_;
+}
+
+sub release {
+    my ($self, $arg) = @_;
+}
+
+sub status {
+    my ($self, $arg) = @_;
+}
+
+sub team_add {
+    my ($self, $arg) = @_;
+}
+
+sub watch {
+    my ($self, $arg) = @_;
+}
+
 1;
