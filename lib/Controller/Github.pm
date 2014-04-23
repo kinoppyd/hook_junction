46a076b73882e5aff384404b55c6037f12948472
Kinoshita.Yasuhiro <kinoshita.yasuhiro@redfox.co.jp>
2014-04-23 20:11:48 +0900

    なんと、GithubのWebhookは当てに行く場所が一カ所だった！
    （事前にきちんと調べましょう）

--- lib/Controller/Github.pm
+++ lib/Controller/Github.pm
@@ -1,80 +1,9 @@
 package Junction::Controller::Github;
 use Mojo::Base 'Mojolicious::Controller';
 
-sub commit_comment {
+sub receive {
     my $self = shift;
-}
-
-sub create {
-    my $self = shift;
-}
-
-sub delete {
-    my $self = shift;
-}
-
-sub deployment_status {
-    my $self = shift;
-}
-
-sub deployment {
-    my $self = shift;
-}
-
-sub fork {
-    my $self = shift;
-}
-
-sub gollum {
-    my $self = shift;
-}
-
-sub issue_comment {
-    my $self = shift;
-}
-
-sub issues {
-    my $self = shift;
-}
-
-sub member {
-    my $self = shift;
-}
-
-sub page_build {
-    my $self = shift;
-}
-
-sub public {
-    my $self = shift;
-}
-
-sub pull_request_review_comment {
-    my $self = shift;
-}
-
-sub pull_request {
-    my $self = shift;
-}
-
-sub push {
-    my $self = shift;
-}
-
-sub release {
-    my $self = shift;
-}
-
-sub status {
-    my $self = shift;
-}
-
-sub team_add {
-    my $self = shift;
-}
-
-sub watch {
-    my $self = shift;
+    my $payload = $self->param('payload');
 }
 
 1;
