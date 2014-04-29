21f9861c80e03f58985e7d2f4e124c1601e439cd
Kinoshita.Yasuhiro <kinoshita.yasuhiro@redfox.co.jp>
2014-04-29 20:55:16 +0900

    Github用のHookReceiver実装を追加

---
+++ lib/Model/HookReceiver/Github.pm
@@ -0,0 +1,11 @@
+package Junction::Model::HookReceiver::Github;
+
+use Mouse;
+with qw/Junction::Model::HookReceiver/;
+
+has 'githubEveng' => (
+    is => 'ro',
+    isa => 'Str',
+    init_arg => 'github_event',
+    required => 1
+);
