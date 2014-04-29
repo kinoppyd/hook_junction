b9559d1afeac1654c763853eb952c4094f292931
Kinoshita.Yasuhiro <kinoshita.yasuhiro@redfox.co.jp>
2014-04-29 22:14:33 +0900

    PostSenderを雑に定義

--- lib/Junction/Model/PostSender.pm
+++ lib/Junction/Model/PostSender.pm
@@ -1,5 +1,6 @@
 package Junction::Model::PostSender;
 use Mouse::Role;
+use Mojo::UserAgent;
 
 requires qw/post/;
 
@@ -7,5 +8,31 @@ has 'hookReceiver' => (
     is => 'ro',
     isa => 'Model::HookReceiver'
 );
+
+has 'entryPoint' => (
+    is => 'rw',
+    isa => 'Str',
+    required => 1
+);
+
+has 'header' => (
+    is => 'rw',
+    isa => 'HashRef',
+    default => +{}
+);
+
+has 'body' => (
+    is => 'rw',
+    isa => 'HashRef',
+    default => +{}
+);
+
+sub post {
+    my $self = shift;
+
+    my $ua = Mojo::UserAgent->new;
+    my $tx = $ua->post( $self->entryPoint => $self->header => form => $self->body );
+    return $tx;
+}
 
 1;
